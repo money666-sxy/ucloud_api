171c117285cd1c1576178bb08aac062362e96d97
money666-sxy <615699752@qq.com>
2021-01-04 21:42:42 +0800

    删除bucket下的所有内容的脚本

---
+++ run.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/ucloud/ufile-sdk-python\
+cd /ufile-sdk-python
+python3 setup.py install
+pip3 install ufile
+python3 https://github.com/money666-sxy/ucloud_api/blob/master/all_delect.py
