04fc2a60181c839bae6bafa342bac00db6f5e364
money666-sxy <615699752@qq.com>
2021-01-04 21:38:20 +0800

    删除bucket下的所有内容

---
+++ all_delect.py
@@ -0,0 +1,26 @@
+from ufile import filemanager
+from ufile import bucketmanager
+from ufile import config
+import json
+
+
+def all_delect(publicBucket, publicKey, privateKey):
+    hello_file = filemanager.FileManager(public_key, private_key)
+    ret, resp = hello_file.getfilelist(public_bucket)
+    while dict(ret)["DataSet"]:
+        for i in dict(ret)["DataSet"]:
+            cloudfile_name = dict(i)["FileName"]
+        #     # while cloudfile_name:
+            hello_file.deletefile(public_bucket, cloudfile_name)
+            # print(cloudfile_name)
+            ret, resp = hello_file.getfilelist(public_bucket)
+    print('删除完成,请移步控制台手动删除bucket')
+
+
+if __name__ == "__main__":
+    public_bucket = input('输入bucket名（不带后缀）：')
+    public_key = input('输入公钥：')
+    private_key = input('输入私钥：')
+    region = input('输入区域：')
+
+    all_delect(public_bucket, public_key, private_key)
