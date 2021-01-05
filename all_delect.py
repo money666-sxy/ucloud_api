9833525ea4243c4bb34a5992482eed42e7253908
money666-sxy <615699752@qq.com>
2021-01-05 09:38:05 +0800

    删除bucket下的所有内容,添加region注释

--- all_delect.py
+++ all_delect.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
     public_bucket = input('输入bucket名（不带后缀）：')
     public_key = input('输入公钥：')
     private_key = input('输入私钥：')
-    region = input('输入区域：')
+    print('地区列表：https://docs.ucloud.cn/api/summary/regionlist')
+    region = input('输入区域(cn-bj)：')
     # 设置上传host后缀,外网可用后缀形如 .cn-bj.ufileos.com（cn-bj为北京地区，其他地区具体后缀可见控制台：对象存储-单地域空间管理-存储空间域名）
     config.set_default(uploadsuffix='.{0}.ufileos.com'.format(region))
     # 设置下载host后缀，普通下载后缀即上传后缀，CDN下载后缀为 .ufile.ucloud.com.cn
