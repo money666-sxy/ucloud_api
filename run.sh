f2c5974b6b23cd77922c2e646b425f733fb1a2dc
money666-sxy <53988160+money666-sxy@users.noreply.github.com>
2021-01-05 21:08:19 +0800

    Delete run.sh
    useless shell

--- run.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/ucloud/ufile-sdk-python\
-cd /ufile-sdk-python
-python3 setup.py install
-pip3 install ufile
-python3 https://github.com/money666-sxy/ucloud_api/blob/master/all_delect.py
